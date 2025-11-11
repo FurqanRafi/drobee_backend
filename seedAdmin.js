d22cf8d72d0885cdc14ace01165733e074627a5a
Muhammad Furqan <furqanf64123@gmail.com>
2025-11-11 17:16:26 +0500

    ok

--- seedAdmin.js
+++ seedAdmin.js
@@ -3,7 +3,9 @@ import D_Admin from "./models/d_AdminSchema.js";
 import mongoose from "mongoose";
 const connectDB = async () => {
   try {
-    await mongoose.connect("mongodb+srv://furqan:furqan123@cluster0.udrs5zb.mongodb.net/?appName=Cluster0");
+    await mongoose.connect(
+      "mongodb+srv://furqan:furqan123@cluster0.udrs5zb.mongodb.net/?appName=Cluster0"
+    );
     console.log("MongoDB connected");
   } catch (error) {
     console.log("MongoDB connection error", error);
@@ -13,14 +15,14 @@ const connectDB = async () => {
 const seedAdmin = async () => {
   try {
     await connectDB();
-    const admin = await D_Admin.findOne({ email: "admin@gmail.com" });
+    const admin = await D_Admin.findOne({ email: "admin@drobee.com" });
     if (admin) {
       console.log("Admin already exists");
       return;
     }
     const hashedPassword = await bcrypt.hash("admin", 10);
     await D_Admin.create({
-      email: "admin@gmail.com",
+      email: "admin@drobee.com",
       password: hashedPassword,
     });
     console.log("Admin created");
