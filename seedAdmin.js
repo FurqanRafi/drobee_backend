6a04ca825c8f9a3eba4dcf9c88bbe26d86614cfd
Muhammad Furqan <furqanf64123@gmail.com>
2025-11-11 17:09:59 +0500

    ok

--- seedAdmin.js
+++ seedAdmin.js
@@ -1,10 +1,18 @@
 import bcrypt from "bcryptjs";
-
-import connectDB from "./config/mongodbConnect.js";
 import D_Admin from "./models/d_AdminSchema.js";
-connectDB();
+import mongoose from "mongoose";
+const connectDB = async () => {
+  try {
+    await mongoose.connect("mongodb+srv://furqan:furqan123@cluster0.udrs5zb.mongodb.net/?appName=Cluster0");
+    console.log("MongoDB connected");
+  } catch (error) {
+    console.log("MongoDB connection error", error);
+    process.exit(1);
+  }
+};
 const seedAdmin = async () => {
   try {
+    await connectDB();
     const admin = await D_Admin.findOne({ email: "admin@gmail.com" });
     if (admin) {
       console.log("Admin already exists");
@@ -21,4 +29,4 @@ const seedAdmin = async () => {
   }
 };
 
-seedAdmin();
+await seedAdmin();
