0af4f91aa42f29828955992262f8ff6ad66c544a
Muhammad Furqan <furqanf64123@gmail.com>
2025-11-13 11:16:02 +0500

    iii

--- models/d_shippingSchema.js
+++ models/d_shippingSchema.js
@@ -2,28 +2,27 @@ import mongoose from "mongoose";
 
 const d_shippingSchema = new mongoose.Schema(
   {
-    // e.g. "Standard Delivery", "Express Delivery", "Cash on Delivery"
+
     name: {
       type: String,
       required: true,
       trim: true,
     },
 
-    // e.g. "3–5 days", "1–2 days", "Pay when received"
     description: {
       type: String,
       required: true,
       trim: true,
     },
 
-    // e.g. 200, 500, 250
+
     price: {
       type: Number,
       required: true,
       min: 0,
     },
 
-    // optional: to check if this shipping method is active or not
+
     isActive: {
       type: Boolean,
       default: true,
